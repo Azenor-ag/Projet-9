b6a6496c59b753bcce21eefa43741662c2363566
Azenor <fabien59153@gmail.com>
2023-02-19 15:41:50 +0100

    debut du hero header

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 console.log("coucou")
-//ajout nominations//
-function nominations(){
-let nomination = document.querySelector("main");
-let oscar = document.createElement("section");
-nomination.appendChild(oscar);
-const imageElement = document.createElement("img");
-imageElement.src = "foce-child/section_finale.png";
-oscar.appendChild(imageElement);
-}
-nominations();
 
 /*vidéo banner*/
-function insertVideo()
-{const banner = document.querySelector(".banner");
-banner.innerHTML = '<video auto play muted loop class="banner"><source src="foce-child/Studio+Koukaki-vidéo+header+sans+son+(1).mp4" type="video/mp4"></video>';
+const addVideo = document.querySelector(".banner");
+    if addVideoListener('DOMContentLoaded',true){
+
+else
+
+    }
+/*banner.innerHTML = <video auto play muted loop class="banner"><source src="<?php echo get_template_directory_uri() . 'foce-child/Studio+Koukaki-vidéo+header+sans+son+(1).mp4'" type="video/mp4"></video>';
+*/
+/*titres   Ok*/
+document.addEventListener('DOMContentLoaded', (event) => {
+ 
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const titres = entry.target.querySelector("h1,h2,h3");
+  
+      if (entry.isIntersecting) {
+        titres.classList.add('toAnim');
+        return; 
+      }
+   
+      titres.classList.remove('toAnim');
+    });
+  });
+  
+  observer.observe(document.querySelector('section'));
+});
+
+
+/*carrousel
+
+   constructor (element, options = {}){
+    this.element = element
+this.options = Object.assign({},{
+    slidesToScroll =  1,
+    slidesVisible = 1},options) 
+let root =querySelector('#characters')}
 }
-insertVideo();
-
-/*titres*/
-
-const scroll = document.querySelector("h1,h2,h3");
-window.addEventListener("scroll", function () {
-    window.classList.add('swipe');/*keyframes swipe*/
+document.addEventListener('scroll', function (){
+    new caroussel(document.querySelector('#characters'),
+    carrousel.addclassList("main-character");
+    let otherCharacters = document.querySelector('other-characters');
+    otherCharacters.addclassList.remove('other-characters');
+    {slideToScroll = 3,
+    slideVisible = 3}
+    )
 })
-
-/*carrousel*/
-
-let newCaroussel
+*/
